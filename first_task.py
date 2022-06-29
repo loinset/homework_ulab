99664052cab00350cf46da9c6ab43cc7030f5452
loins <inset.work@ya.ru>
2022-06-29 15:33:27 +0400

    Five task push

--- first_task.py
+++ first_task.py
@@ -1,14 +1,18 @@
 import re
+from urllib.parse import urlparse
+
+MOST_OFTEN = ('COM', 'NET', 'ORG', 'INT', 'EDU', 'GOV', 'MIL', 'ARPA',
+              'INFO', 'BIZ', 'NAME', 'COOP', 'MUSEUM', 'AERO', 'PRO', 'БЕЛ', 'СРБ', 'МОН')
 
 
 def domain_name(url):
-    all_regular = re.findall('[A-zА-я0-9.-]+', url)
-    for example in all_regular:
-        if example[-1] != '.' and example[0] != '.' and example.find('.') != -1:
-            return example.split('.')[-2]
+    url_peace = urlparse(url)
+    all_domain = url_peace[1].split('.')
+    for domain in reversed(all_domain[:-1]):
+        if domain.upper() not in MOST_OFTEN and len(domain) > 2:
+            return domain
     return 'Err'
 
 
 enters_url = input()
-
 print(domain_name(enters_url))
