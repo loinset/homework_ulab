70bcef83ebb813be2e54d6f75b7963da2eedf1ae
loins <inset.work@ya.ru>
2022-06-30 15:15:36 +0400

    optimized zero search algorithm

--- third_task.py
+++ third_task.py
@@ -2,18 +2,23 @@ import math
 
 
 def zeros(number):
-    counter = 0
-    for i in range(number + 1):
-        power = 1
-        middle_counter = 0
-        while pow(5, power) <= i:
-            if i % pow(5, power) == 0:
-                middle_counter = power
-            else:
-                break
-            power += 1
-        counter += middle_counter
-    return counter
+    power = 1
+    fives_counter = [0] * 101
+    # 102 for the correct iteration of a cycle of size 100 from 1 to 100
+    while power < number:
+        if pow(5, power) > number:
+            break
+        power += 1
+    power -= 1
+    counter = power
+    while power:
+        fives_counter[power] = number // pow(5, power)
+        power -= 1
+    result = 0
+    while counter:
+        result += fives_counter[counter]
+        counter -= 1
+    return result
 
 
 enter_number = int(input())
